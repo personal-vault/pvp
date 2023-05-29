d9cb08e69044799f84aa2601e94e606b84b9c139
Lucian Daniliuc <dlucian@gmail.com>
2023-05-29 22:33:02 +0300

    add checksum

--- roadrunner/src/Task/ScanFile.php
+++ roadrunner/src/Task/ScanFile.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Memorelia\Task;
 
+use InvalidArgumentException;
 use RoadRunner\Logger\Logger;
+use RuntimeException;
 
 class ScanFile
 {
@@ -15,6 +17,20 @@ class ScanFile
     public function run(string $id, string $payload): void
     {
         $settings = json_decode($payload);
-        $this->logger->info('ID ' . $id . ' ' . $settings->filename . PHP_EOL);
+        assert($settings->filename !== null && is_string($settings->filename));
+        $start = microtime(true);
+        $checksum = $this->calculate_checksum($settings->filename);
+        $time = microtime(true) - $start;
+        $this->logger->info('|> ' . $checksum . ' ' . sprintf('%02.2f ', $time) . $settings->filename . ' ' . PHP_EOL);
+    }
+
+    private function calculate_checksum($file_path) {
+        $output = '';
+        $exit_code = null;
+        exec('sha256sum ' . escapeshellarg($file_path), $output, $exit_code);
+        if ($exit_code !== 0) {
+            throw new InvalidArgumentException('Invalid file path: ' . $file_path);
+        }
+        return explode(' ', $output[0])[0];
     }
 }
